f8713fa2ef6d1807556453e6def5edb24ef32ec7
majmjr1 <tekrecruiter13@gmail.com>
2025-09-02 19:10:47 -0400

    9th commit

--- scripts/index.js
+++ scripts/index.js
@@ -33,13 +33,27 @@ const initialCards = [
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileFormEl = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostFormEl = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#profile-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#profile-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const profileImageEl = document.querySelector(".profile__image");
+const profileCaptionEl = document.querySelector("profile__caption");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -53,6 +67,27 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(newPostImageInput);
+  console.log(newPostCaptionInput);
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileFormEl.addEventListener('submit', handleProfileFormSubmit);
+newPostFormEl.addEventListener('submit', handleAddCardSubmit);
 
 initialCards.forEach(function (card) {
   console.log(card.name);
